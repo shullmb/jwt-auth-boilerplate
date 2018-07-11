22cc296fd7cc4598b10908b89c13b3585e6014b9
shullmb <shullmb@gmail.com>
2018-07-11 14:53:15 -0700

    react: set initial state for auth

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: '',
+      user: null
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
