fb81b94a28125cd89f552c109a671bc63913d8b5
shullmb <shullmb@gmail.com>
2018-07-11 15:08:19 -0700

    added checkForLocalToken in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,36 @@ class App extends Component {
       token: '',
       user: null
     }
+    this.checkForLocalToken = this.checkForLocalToken.bind(this);
   }
+
+  checkForLocalToken() {
+    //Look for token in local storage
+    let token = localStorage.getItem('mernToken');
+    if (!token) {
+      // There was no token
+      // remove mernToken from local storage just in case corrupted, replace etc
+      localStorage.removeItem('mernToken') 
+      this.setState({
+        token: '',
+        user: null
+      })
+    } else {
+      // token found in local storage
+      // send it to the back to be verified
+      axios.post('/auth/me/from/token', {
+        token
+      }).then( results => {
+        // put the token in local storage
+        localStorage.setItem('mernToken', results.data.token);
+        this.setState({
+          token: results.data.token,
+          user: results.data.user
+        })
+      }).catch( err => console.log(err))
+    }
+  }
+
   render() {
     return (
       <div className="App">
